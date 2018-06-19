7c8ed2f631e553063117f0e7f13b49450373bbf3
Franklin Liu <liufran1@uchicago.edu>
2018-06-18 19:07:36 -0500

    First pass at June 2018 Data Viz battle, need to adjust colors and clean dates

---
+++ Roman-Emperors/README.md
@@ -0,0 +1,3 @@
+Submission for `r/dataisbeautiful`'s June 2018 [DataViz Battle](https://www.reddit.com/r/dataisbeautiful/comments/8ogk4k/battle_dataviz_battle_for_the_month_of_june_2018/), a visualization of the reigns of Roman Emperors.
+
+Data from [here](https://github.com/zonination/emperors/blob/master/emperors.csv)
---
+++ Roman-Emperors/emperors_analysis.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(data.table)
+library(plotly)
+
+#Read in data
+romanEmperors<-fread('emperors.csv')
+
+#Clean dates
+romanEmperors[,birth:=as.Date(birth)]
+romanEmperors[,death:=as.Date(death)]
+romanEmperors[,reign.start:=as.Date(reign.start)]
+romanEmperors[,reign.end:=as.Date(reign.end)]
+
+  #Change dates to BCE
+romanEmperors[index<5, year(romanEmperors$birth) := -year(romanEmperors$birth)]
+
+
+#Set factor levels
+romanEmperors$name<-factor(romanEmperors$name, levels = romanEmperors$name)
+
+#Plot
+reignsPlot<-ggplot() +
+  geom_segment(data = romanEmperors, aes(x = birth, xend = death, y = name, yend = name), size = 2)+
+  geom_segment(data = romanEmperors, aes(x = reign.start, xend = reign.end, y = name, yend = name, color= dynasty), size = 2)+
+  xlab('Year') + 
+  ylab('Emperor') +
+  theme_classic()
+
+plotly::ggplotly(reignsPlot)
+
+#api_create(p, filename = "roman-emperors")
