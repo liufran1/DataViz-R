81f59b02fda7a6be5260a9119c2adb45ebfc69be
Franklin Liu <liufran1@uchicago.edu>
2018-06-21 12:54:44 -0500

    Added rise and death causes

--- Roman-Emperors/emperors_analysis.R
+++ Roman-Emperors/emperors_analysis.R
@@ -13,11 +13,16 @@ romanEmperors[,birth:=as.Date(birth)]
 romanEmperors[,death:=as.Date(death)]
 romanEmperors[,reign.start:=as.Date(reign.start)]
 romanEmperors[,reign.end:=as.Date(reign.end)]
-
+romanEmperors[is.na(birth), birth:=reign.start]
 #Change dates to BCE
 romanEmperors[,birth.year:=year(birth)]
 romanEmperors[index<5,birth:=birth - birth.year*365*2 - floor(birth.year/2)]
 romanEmperors[index==6,birth:=birth - birth.year*365*2 - floor(birth.year/2)]
+romanEmperors[index==1,reign.start:=reign.start - year(reign.start)*365*2 - floor(year(reign.start)/2)]
+
+#Simplify Death Causes, 'Natural Causes' == 'Peaceful
+romanEmperors[,Power_Transfer:='Violent']
+romanEmperors[cause=='Natural Causes' ,Power_Transfer:='Peaceful']
 
 #Set factor levels
 
@@ -25,17 +30,32 @@ romanEmperors$name<-factor(romanEmperors$name, levels = romanEmperors$name)
 romanEmperors$dynasty<-factor(romanEmperors$dynasty, levels = unique(romanEmperors$dynasty))
 
 #TODO: reverse factors
+beginningYear<-as.Date('0100-01-01')
+beginningYear<-beginningYear - year(beginningYear)*365*2
 
 #Plot
-
-reignsPlot<-ggplot() +
+#TODO: Manually adjust size
+reignsPlotBack<-ggplot() +
+  geom_segment(data = romanEmperors, aes(x = beginningYear, xend = birth, y = name, yend = name, color = rise), size = 2)+
+  geom_segment(data = romanEmperors, aes(x = death, xend = as.Date('0450-01-01'), y = name, yend = name, color = Power_Transfer), size = 2)+
   geom_segment(data = romanEmperors, aes(x = birth, xend = death, y = name, yend = name), size = 2)+
+  geom_segment(data = romanEmperors, aes(x = birth+500, xend = death-500, y = name, yend = name), size = 1.5, color = 'white')+ #hack to get around geom_segment not accepting fill param
   geom_segment(data = romanEmperors, aes(x = reign.start, xend = reign.end, y = name, yend = name, color= dynasty), size = 2)+
   xlab('Year') + 
   ylab('Emperor') +
   theme_classic()
 
-plotly::ggplotly(reignsPlot)
+reignsPlotSide<-ggplot() +
+  geom_segment(data = romanEmperors, aes(x = as.Date('0400-01-01'), xend = as.Date('0450-01-01'), y = name, yend = name, color = rise), size = 2)+
+  geom_segment(data = romanEmperors, aes(x = as.Date('0450-01-01'), xend = as.Date('0500-01-01'), y = name, yend = name, color = Power_Transfer), size = 2)+
+  geom_segment(data = romanEmperors, aes(x = birth, xend = death, y = name, yend = name), size = 2)+
+  geom_segment(data = romanEmperors, aes(x = birth+500, xend = death-500, y = name, yend = name), size = 1.5, color = 'white')+ #hack to get around geom_segment not accepting fill param
+  geom_segment(data = romanEmperors, aes(x = reign.start, xend = reign.end, y = name, yend = name, color= dynasty), size = 2)+
+  xlab('Year') + 
+  ylab('Emperor') +
+  theme_classic()
+
+#plotly::ggplotly(reignsPlot)
 
 #gganimate doesn't do cumulative with factors
 
@@ -46,9 +66,8 @@ reignsPlot<-ggplot() +
   ylab('Emperor') +
   theme_classic()
 
-gganimate(reignsPlot, "romanemperors.gif",interval = 0.4, ani.width = 1200, ani.height = 800)
+#gganimate(reignsPlot, "romanemperors.gif",interval = 0.4, ani.width = 1200, ani.height = 800)
 
-#TODO: plots of birthplaces and cause of death/ascension
-#TODO: compare cause of death with ascension
+#TODO: plots of birthplaces
 
 #api_create(reignsPlot, filename = "roman-emperors")
